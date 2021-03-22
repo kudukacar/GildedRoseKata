a50eaff6b82cca61a8031ac4c0a697205eecbf05
Manju <manju.thomas083@gmail.com>
2021-03-22 19:53:23 -0400

    changed updaters test

--- src/test/java/UpdatersTest.java
+++ src/test/java/UpdatersTest.java
@@ -10,7 +10,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class UpdatersTest {
     @Test
     void itCreatesAMapOfUpdaters() {
-        Updateable updater = new SimpleUpdater();
+        Updateable updater = new ItemUpdater();
         String itemName = "item";
         Updaters updaters = new Updaters.Builder()
                 .addUpdater(itemName, updater)
@@ -22,11 +22,13 @@ class UpdatersTest {
 
     @Test
     void itUpdatesAnItemWithTheRightUpdater() {
-        Item item = new Item("item", 0, 0);
-        Updateable updater = new SimpleUpdater();
         String itemName = "item";
+        String anotherItemName = "anotherItem";
+        Item item = new Item(itemName, 0, 0);
+        Item anotherItem = new Item(anotherItemName, 0, 0);
         Updaters updaters = new Updaters.Builder()
-                .addUpdater(itemName, updater)
+                .addUpdater(itemName, new ItemUpdater())
+                .addUpdater(anotherItemName, new AnotherItemUpdater())
                 .build();
         updaters.update(item);
         assertEquals(1, item.sellIn);
@@ -34,11 +36,17 @@ class UpdatersTest {
     }
 
 
-    private class SimpleUpdater implements Updateable {
+    private class ItemUpdater implements Updateable {
         @Override
         public void update(Item item) {
             item.sellIn += 1;
             item.quality += 1;
+        }
+    }
+
+    private class AnotherItemUpdater implements Updateable {
+        @Override
+        public void update(Item item) {
         }
     }
 
