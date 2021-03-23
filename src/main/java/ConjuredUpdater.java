2d1516d46e34c578c7be75e9eea172d0055892b3
Jimjule <roadknightj@gmail.com>
2021-03-23 11:42:52 +0000

    Removes redundant if statement

--- src/main/java/ConjuredUpdater.java
+++ src/main/java/ConjuredUpdater.java
@@ -2,9 +2,6 @@ public class ConjuredUpdater implements Updateable{
     @Override
     public void update(Item item) {
         item.sellIn -= 1;
-        if (item.quality <= 0) {
-            return;
-        }
         if (item.sellIn > 0) {
             item.quality = Math.max(0, item.quality -= 2);
         } else {
