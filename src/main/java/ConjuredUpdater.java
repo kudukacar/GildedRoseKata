9505a9dad4b3f7fc9b2d8344dbba5266bbf7ba7a
Jimjule <roadknightj@gmail.com>
2021-03-23 11:30:46 +0000

    Implements conjured items

---
+++ src/main/java/ConjuredUpdater.java
@@ -0,0 +1,14 @@
+public class ConjuredUpdater implements Updateable{
+    @Override
+    public void update(Item item) {
+        item.sellIn -= 1;
+        if (item.quality <= 0) {
+            return;
+        }
+        if (item.sellIn > 0) {
+            item.quality = Math.max(0, item.quality -= 2);
+        } else {
+            item.quality = Math.max(0, item.quality -= 4);
+        }
+    }
+}
