7702e08932a6cbc2f7f558f56714162dd7f824b0
Jimjule <roadknightj@gmail.com>
2021-04-01 13:31:44 +0100

    Verifies that itemUpdater calls repo update method

--- src/test/java/com/example/gildedrose/service/ItemUpdaterTest.java
+++ src/test/java/com/example/gildedrose/service/ItemUpdaterTest.java
@@ -4,6 +4,11 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.ArrayList;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
 @SpringBootTest
 class ItemUpdaterTest {
     @Mock
@@ -13,5 +18,6 @@ class ItemUpdaterTest {
     public void updatesTheItemRepository() {
         ItemUpdater itemUpdater = new ItemUpdater(itemRepository);
         itemUpdater.performUpdate();
+        verify(itemRepository, times(1)).update(new ArrayList<>());
     }
 }
